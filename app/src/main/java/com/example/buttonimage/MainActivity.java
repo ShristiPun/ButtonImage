1f2415aea86e2a694bd64bd9e335d3627be7dc73
ShristiPun <55776369+ShristiPun@users.noreply.github.com>
2019-11-30 23:18:15 +0545

    jj

--- app/src/main/java/com/example/buttonimage/MainActivity.java
+++ app/src/main/java/com/example/buttonimage/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.clickimage;
+package com.example.buttonimage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
--- app/src/test/java/com/example/buttonimage/ExampleUnitTest.java
+++ app/src/test/java/com/example/buttonimage/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.clickimage;
+package com.example.buttonimage;
 
 import org.junit.Test;
 
